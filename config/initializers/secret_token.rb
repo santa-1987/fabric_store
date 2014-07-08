2ae1570378b0bfb1e26b50642b1651ac763bffe7
Santosh Mohanty <smohanty@accordionsystems.com>
2014-07-08 11:57:10 +0530

    Added Fabric Store files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FabricStore::Application.config.secret_token = '3c2f9b674035026bbc05e50f581dcf89da8aff5fbf60ecf52dc6700218f7075af80095d34da7e2eddc702a4fb6aaebfc4238800972f81bc19419da764b49e9c9'
